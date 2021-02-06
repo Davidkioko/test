500657c81a163cf610573ec8d9f70e2d771329ab
Your Name <you@example.com>
2021-02-06 17:35:58 +0300

    Python Operators

---
+++ pythonoperators.py
@@ -0,0 +1,42 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+x=13
+y=5
+print(x/y)
+print(x//y)
+q=x+y
+print(q)
+s=x*y
+print (s)
+c=x%y
+print(c)
+r="My name is david"
+v=" I learn at zetech"
+c=r+v
+print(c)
+a=13
+b=33
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+a="true"
+b="false"
+
+print (a and b)
+print (a or b)
+print (not a)
+x="python"
+y="variables"
+print (x>=y)
+
+a=10
+b=4
+print (a&b)
+print (a | b)
+print (~a)
+print (a ^ b)
+print (a>>2)
+print (a<<2)
